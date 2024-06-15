6472bec5679fec470d12b7d6a9a0a766d06014b7
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-14 23:01:58 -0400

    Added helpers

---
+++ src/utils/helpers.ts
@@ -0,0 +1,5 @@
+export const formatBalance = (rawBalance: string) => (parseInt(rawBalance) / 1000000000000000000).toFixed(2)
+
+export const formatChainAsNum = (chainIdHex: string) => parseInt(chainIdHex)
+
+export const formatAddress = (addr: string | undefined) => `${addr?.substring(0, 8)}...`
