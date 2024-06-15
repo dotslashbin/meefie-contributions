00d2eed1da416031f0d974d7ad5b417e326e30a6
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-14 22:42:54 -0400

    Cleaned up and added icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 // @ts-ignore
-import { addContributor } from '@/services/firebase';
-import {Contributor} from "@/types";
 
 export default function Index() {
 
-    const handleSubmit = async () => {
-
-        const data: Contributor = ({
-            name: 'Ax',
-            email: 'Bx',
-            wallet: 'Cx',
-            transaction_hash: 'Dx',
-        });
-
-        await addContributor(data);
-    }
-
     return (
         <div>
-            <button onClick={handleSubmit}>Test save</button>
+            the index page
         </div>
     )
 }
