1b409b52f5d2bdc2958aaa88fe58d9c9e2301291
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-17 17:59:36 -0400

    added reading of state from component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { useStore }  from '../context/StoreContext'
 
 export default function Index() {
+
+    const { state } = useStore()
+
+    console.log('#DEBUG state: ', state)
 
     return (
         <div>
             <NavBar />
             <div>
-                this is where the form is
+                this is where the form is {state.account}
             </div>
         </div>
     )
