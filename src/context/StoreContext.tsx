674a1a7c35144a546a2d6330fc70a3eccb337313
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-17 17:58:33 -0400

    Implemented store context

---
+++ src/context/StoreContext.tsx
@@ -0,0 +1,50 @@
+// context/StoreContext.tsx
+import React, { createContext, useContext, useReducer, ReactNode } from 'react'
+
+interface State {
+    count: number,
+    account: string
+}
+
+type Action = { type: 'SET_ACCOUNT', payload: string };
+
+const initialState: State = {
+    count: 0,
+    account: ''
+};
+
+const reducer = (state: State, action: Action ): State => {
+    switch (action.type) {
+        case 'SET_ACCOUNT':
+            return { ...state, account: action.payload };
+        default:
+            return state;
+    }
+};
+
+interface StoreContextProps {
+    state: State;
+    dispatch: React.Dispatch<Action>;
+}
+
+const StoreContext = createContext<StoreContextProps | undefined>(undefined);
+
+const StoreProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <StoreContext.Provider value={{ state, dispatch }}>
+            {children}
+        </StoreContext.Provider>
+    );
+};
+
+const useStore = (): StoreContextProps => {
+    const context = useContext(StoreContext);
+    if (!context) {
+        throw new Error('useStore must be used within a StoreProvider');
+    }
+    return context;
+};
+
+export { StoreProvider, useStore };
