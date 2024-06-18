4dd4fd4491be0c67b1aaf3c5b22568d99e372091
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-18 17:34:45 -0400

    Added definitions

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
-export const TOKEN_ADDRESS = '0x7169D38820dfd117C3FA1f22a697dBA58d90BA06'
+export const TOKEN_ADDRESS: string = '0x7169D38820dfd117C3FA1f22a697dBA58d90BA06'
 
-export const TOKEN_ABI = [
+export const TOKEN_ABI: string[] = [
     "function balanceOf(address owner) view returns (uint256)"
 ]
 
-export const MIN_DONATION = 100
+export const MIN_DONATION: number = 100
