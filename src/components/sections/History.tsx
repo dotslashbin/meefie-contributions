a6da7824fccad943c79cdfd8a76c4ce24f03b84a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-24 18:54:42 -0400

    Refactored to reduce variables

--- src/components/sections/History.tsx
+++ src/components/sections/History.tsx
@@ -10,31 +10,12 @@ export default function History(): ReactElement {
 
     const { state, dispatch } = useStore()
     const [ contributions, setContributions ] = useState<Contribution[]>([])
-
-    useEffect(() => {
-
-        if(state.history.length == 0) {
-            getDonations(COLLECTION_NAME, 'user_wallet', state.account).then((documents: DocumentData) => {
-                const data = documents.map((document: Contribution) => document)
-                setContributions(data)
-
-                dispatch({ type: 'SET_HISTORY', payload: data })
-            })
-        } else  {
-            // GEt from store
-            console.log('#DEBUG ..inside sotre: "', state.history)
-        }
-
-
-
-    }, []);
-
     return (
         <div>
             <h4>Your contributions:</h4>
-            { contributions.length ? (
+            { state.history.length > 0 ? (
                 <div>
-                    { contributions.map((contribution: Contribution) => (<HistoryItem key={contribution.transaction_hash} contribution={contribution} />))}
+                    { state.history.map((contribution: Contribution) => (<HistoryItem key={contribution.transaction_hash} contribution={contribution} />))}
                 </div>
             ): (<span>You have not made any contribution yet</span>)}
         </div>
