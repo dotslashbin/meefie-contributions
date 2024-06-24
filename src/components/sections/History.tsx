9eb579c46a9cdfb80a249f06c6f0307b02ee9618
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-24 17:39:33 -0400

    implemented reading of history

--- src/components/sections/History.tsx
+++ src/components/sections/History.tsx
@@ -1,25 +1,32 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { useStore } from '@/context/StoreContext'
 import { getDonations} from "@/services/Firebase"
 import { DocumentData } from "firebase/firestore";
 import { COLLECTION_NAME } from "@/utils/db";
+import { Contribution } from "@/types";
 
 export default function History(): ReactElement {
 
     const { state } = useStore()
+    const [ contributions, setContributions ] = useState<Contribution[]>([])
 
     useEffect(() => {
         if(!state.history) {
             console.log("#FETCH the hhistory ....")
             getDonations(COLLECTION_NAME, 'user_wallet', state.account).then((documents: DocumentData) => {
-                console.log('here are theh data: ', documents);
+                setContributions(documents.map((document: Contribution) => document))
             })
         }
     }, [state]);
 
     return (
         <div>
-            Show the history here
+            <h4>Your contributions:</h4>
+            { contributions.length ? (
+                <ol>
+                    { contributions.map((contribution: Contribution) => (<li>{ contribution.transaction_hash }</li>))}
+                </ol>
+            ): (<span>You have not made any contribution yet</span>)}
         </div>
     )
 }
