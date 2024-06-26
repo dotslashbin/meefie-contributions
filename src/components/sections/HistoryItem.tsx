25b6effa73fa9b8c218ac932e5516060e8822015
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-26 16:45:39 -0400

    Added date of transaction

--- src/components/sections/HistoryItem.tsx
+++ src/components/sections/HistoryItem.tsx
@@ -4,10 +4,10 @@ import {formatAddress} from "@/utils/helpers";
 import {BLOCK_EXPLORER_URL, VALUE_PER_USDT} from "../../../config";
 
 // PDF Generation
-import { pdf } from '@react-pdf/renderer';
-import ContributionCertificate from "@/documents/ContributionCertificate";
-import { saveAs } from 'file-saver';
-
+import { pdf } from '@react-pdf/renderer'
+import ContributionCertificate from "@/documents/ContributionCertificate"
+import { saveAs } from 'file-saver'
+import moment from 'moment'
 
 interface ContributionItemProps {
     contribution: Contribution
@@ -16,24 +16,29 @@ export default function ContributionItem({ contribution }: ContributionItemProps
 
     const [loading, setLoading] = useState(false);
 
-    const generatePdfDocument = async () => {
-        setLoading(true);
-
-        // @ts-ignore
-        const tokenAmount = parseFloat(contribution.amount) / VALUE_PER_USDT
-
-        const blob = await pdf(<ContributionCertificate contributor={contribution.name} contribution={contribution.amount} token_amount={tokenAmount.toString()} wallet={contribution.destination_wallet}/>).toBlob();
-        saveAs(blob, 'example.pdf');
-        setLoading(false);
-    };
+    // const generatePdfDocument = async () => {
+    //     setLoading(true);
+    //
+    //     // @ts-ignore
+    //     const tokenAmount = parseFloat(contribution.amount) / VALUE_PER_USDT
+    //
+    //     const blob = await pdf(<ContributionCertificate contributor={contribution.name} contribution={contribution.amount} token_amount={tokenAmount.toString()} wallet={contribution.destination_wallet}/>).toBlob();
+    //     saveAs(blob, 'example.pdf');
+    //     setLoading(false);
+    // };
 
     return (
-        <div className="bg-white text-black my-3.5 rounded p-1 font-thin text-xs">
+        <div className="bg-white text-black my-3.5 rounded p-1 font-thin text-xs max-w-full">
             <div>
-                tx Hash:
-                <a href={`${ BLOCK_EXPLORER_URL }${contribution.transaction_hash}`}>{ formatAddress(contribution.transaction_hash ) }</a>
+                Transaction on: {moment(contribution.transaction_date.toDate()).format('MMMM Do YYYY, h:mma')}
             </div>
-            <div>Amount: { contribution.amount }</div>
+            <div>
+                Hash:
+                <a className="text-decoration-line: underline text-blue-800 cursor-pointer" target="_blank"
+                   href={`${BLOCK_EXPLORER_URL}${contribution.transaction_hash}`}>{formatAddress(contribution.transaction_hash)}</a>
+            </div>
+            <div>Amount: {contribution.amount} USDT</div>
+
         </div>
     )
 }
