2b8eb7f2f22eef61b1dfb790d4cc057fa02e2c04
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-06-28 15:49:49 -0400

    Broken down components and updated version

---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => (
+    <footer className="bg-yellow-500 text-blue p-4 text-center">
+        <p>MeeFie © 2024. All Rights Reserved.</p>
+        <span className="text-xs">version 1.0.0</span>
+    </footer>
+)
+
+export default Footer
